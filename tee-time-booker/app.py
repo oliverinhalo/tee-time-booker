b369086a406b988a92944b386ca937c3d6c3f48a
oliverinhalo <89742362+oliverinhalo@users.noreply.github.com>
2025-10-30 20:00:24 +0000

    Set up Flask app with index route

--- tee-time-booker/app.py
+++ tee-time-booker/app.py
@@ -1,3 +1,12 @@
 import os
-import flask
-import 
+from flask import Flask, render_template
+import tee_time_booker
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
